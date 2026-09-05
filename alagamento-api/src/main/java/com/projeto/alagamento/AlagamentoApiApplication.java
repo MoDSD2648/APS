ef041f204ac13d5018cbf379e60edaa180df4422
MonaizeDutra <147675833+MoDSD2648@users.noreply.github.com>
2026-09-04 21:04:43 -0300

    alagamento-api estrutura

---
+++ alagamento-api/src/main/java/com/projeto/alagamento/AlagamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.alagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlagamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlagamentoApiApplication.class, args);
+	}
+
+}
---
+++ alagamento-api/src/test/java/com/projeto/alagamento/AlagamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.alagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlagamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
