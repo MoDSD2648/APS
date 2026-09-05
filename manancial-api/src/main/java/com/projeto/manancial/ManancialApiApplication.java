2ebe64090a9fd94f84cd9091b5a6665aefbe706b
MonaizeDutra <147675833+MoDSD2648@users.noreply.github.com>
2026-09-04 21:11:54 -0300

    manancial e transito

---
+++ manancial-api/src/main/java/com/projeto/manancial/ManancialApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.manancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManancialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManancialApiApplication.class, args);
+	}
+
+}
---
+++ manancial-api/src/test/java/com/projeto/manancial/ManancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.manancial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
